b5b5e8a66604557ce2892d4f3077fefa980d9af7
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-04 19:40:56 +0530

    MODIFIED: add functionality to find maximum float value

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,9 +1,22 @@
 package com.bridgelabz;
 
 public class Maximum {
-
+    //To find maximum value
     public Integer MaximumInteger(Integer num1, Integer num2, Integer num3) {
-        Integer maximumValue = 0;
+        Integer maximumValue;
+        if(num1.compareTo(num2) > 0)
+            maximumValue = num1;
+        else
+            maximumValue = num2;
+
+        if (maximumValue.compareTo(num3) > 0)
+            return maximumValue;
+        else
+            return num3;
+    }
+    //To find maximum value
+    public Float MaximumFloat(Float num1, Float num2, Float num3) {
+        Float maximumValue;
         if(num1.compareTo(num2) > 0)
             maximumValue = num1;
         else
--- src/test/java/com/bridgelabz/TestMaximum.java
+++ src/test/java/com/bridgelabz/TestMaximum.java
@@ -10,19 +10,42 @@ public class TestMaximum {
     //TC 1.1
     @Test
     public void givenInteger_whenFindMaximumAtPosition1_returnMaximum() {
-        Integer maxNumber = maximum.MaximumInteger(7,6,4);
+        Integer maxNumber = maximum.MaximumInteger(7, 6, 4);
         Assert.assertEquals((Integer)7 , maxNumber);
     }
+
     //TC 1.2
     @Test
     public void givenInteger_whenFindMaximumAtPosition2_returnMaximum() {
-        Integer maxNumber = maximum.MaximumInteger(6,7,4);
+        Integer maxNumber = maximum.MaximumInteger(6, 7, 4);
         Assert.assertEquals((Integer)7, maxNumber);
     }
+
     //TC 1.3
     @Test
     public void givenInteger_whenFindMaximumAtPosition3_returnMaximum() {
-        Integer maxNumber = maximum.MaximumInteger(6,4,7);
+        Integer maxNumber = maximum.MaximumInteger(6, 4, 7);
         Assert.assertEquals((Integer)7, maxNumber);
+    }
+
+    //TC for Float
+    //TC 2.1
+    @Test
+    public void givenFloat_whenFindMaximumAtPosition1_returnMaximum() {
+        Float maxNumber = maximum.MaximumFloat(7.3f, 6.3f, 4.3f);
+        Assert.assertEquals((Float)7.3f, maxNumber);
+    }
+    //TC 2.2
+    @Test
+    public void givenFloat_whenFindMaximumAtPosition2_returnMaximum() {
+        Float maxNumber = maximum.MaximumFloat(6.3f, 7.3f, 4.3f);
+        Assert.assertEquals((Float)7.3f, maxNumber);
+    }
+
+    //TC 2.3
+    @Test
+    public void givenFloat_whenFindMaximumAtPosition3_returnMaximum() {
+        Float maxNumber = maximum.MaximumFloat(6.3f, 4.3f, 7.3f);
+        Assert.assertEquals((Float)7.3f, maxNumber);
     }
 }
